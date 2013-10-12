414c04f8adaa767552a73437b04452507f2679c4
lanlan <lanlan@lanlan-PC>
2013-10-12 10:47:37 +0800

    refactoring

--- src/com/cis350/sleeptracker/ModifyTimeActivity.java
+++ src/com/cis350/sleeptracker/ModifyTimeActivity.java
@@ -12,55 +12,81 @@ import android.widget.DatePicker;
 import android.widget.TimePicker;
 
 public class ModifyTimeActivity extends Activity {
-	private TimePicker mTimePicker;
-	private DatePicker mDatePicker;
-	private SleepLogHelper mSleepLogHelper;
-	private boolean mIsSleepTime;
-	private long mAsleepTime;
+    private TimePicker mTimePicker;
+    private DatePicker mDatePicker;
+    private SleepLogHelper mSleepLogHelper;
+    private boolean mIsSleepTime;
+    private long mAsleepTime;
 	
-	@Override
-	protected void onCreate(Bundle savedInstanceState) {
-		super.onCreate(savedInstanceState);
-		requestWindowFeature(Window.FEATURE_NO_TITLE);
-		setContentView(R.layout.activity_modify_time);
-		
-		mTimePicker = (TimePicker) findViewById(R.id.time_picker);
-		mDatePicker = (DatePicker) findViewById(R.id.date_picker);
-		mSleepLogHelper = new SleepLogHelper(this);
-		mIsSleepTime = false;
-		mAsleepTime = getIntent().getLongExtra(SleepLogHelper.ASLEEP_TIME, 0);
-		long originalTime = getIntent().getLongExtra(SleepLogHelper.AWAKE_TIME, -1);
-		if (originalTime == -1) {
-			mIsSleepTime = true;
-			originalTime = mAsleepTime;
-		}
-		GregorianCalendar cal = new GregorianCalendar();
-		cal.setTimeInMillis(originalTime);
-		mTimePicker.setCurrentHour(cal.get(Calendar.HOUR_OF_DAY));
-		mTimePicker.setCurrentMinute(cal.get(Calendar.MINUTE));
-		mDatePicker.init(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), null);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+	super.onCreate(savedInstanceState);
+	requestWindowFeature(Window.FEATURE_NO_TITLE);
+	setContentView(R.layout.activity_modify_time);
+	/*
+	mTimePicker = (TimePicker) findViewById(R.id.time_picker);
+	mDatePicker = (DatePicker) findViewById(R.id.date_picker);
+	*/
+	mSleepLogHelper = new SleepLogHelper(this);
+	mIsSleepTime = false;
+	mAsleepTime = getIntent().getLongExtra(SleepLogHelper.ASLEEP_TIME, 0);
+	long originalTime = getIntent().getLongExtra(SleepLogHelper.AWAKE_TIME, -1);
+	if (originalTime == -1) {
+	    mIsSleepTime = true;
+	    originalTime = mAsleepTime;
 	}
+	setTimePicker(originalTime);
+	// GregorianCalendar cal = new GregorianCalendar();
+	// cal.setTimeInMillis(originalTime);
+	// mTimePicker.setCurrentHour(cal.get(Calendar.HOUR_OF_DAY));
+	// mTimePicker.setCurrentMinute(cal.get(Calendar.MINUTE));
+	// mDatePicker.init(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), null);
+    }
+    private void setTimePicker(long orgtime) {
+	mTimePicker = (TimePicker) findViewById(R.id.time_picker);
+	mDatePicker = (DatePicker) findViewById(R.id.date_picker);
+
+	GregorianCalendar cal = new GregorianCalendar();
+	cal.setTimeInMillis(orgtime);
+	mTimePicker.setCurrentHour(cal.get(Calendar.HOUR_OF_DAY));
+	mTimePicker.setCurrentMinute(cal.get(Calendar.MINUTE));
+	mDatePicker.init(cal.get(Calendar.YEAR), cal.get(Calendar.MONTH), cal.get(Calendar.DAY_OF_MONTH), null);
+    }
+    public void onClickSave(View view) {
+	GregorianCalendar cal = new GregorianCalendar(mDatePicker.getYear(), mDatePicker.getMonth(),
+						      mDatePicker.getDayOfMonth(), mTimePicker.getCurrentHour(),
+						      mTimePicker.getCurrentMinute());
+	long time = cal.getTimeInMillis();
+	/*	if (mIsSleepTime) {
+	    mSleepLogHelper.updateAsleepTime(mAsleepTime, time);
+	    Intent intent = new Intent();
+	    intent.putExtra(SleepLogHelper.ASLEEP_TIME, time);
+	    setResult(RESULT_OK, intent);
+	} else {
+	    mSleepLogHelper.updateAwakeTime(mAsleepTime, time);
+	    Intent intent = new Intent();
+	    intent.putExtra(SleepLogHelper.AWAKE_TIME, time);
+	    setResult(RESULT_OK, intent);
+	}
+	*/
+	updateTime(mIsSleepTime, time);
+	finish();
+    }
+    private void updateTime(boolean isSlptime, long time) {
+	Intent intent = new Intent();
+	if (isSlptime) {
+	    mSleepLogHelper.updateAsleepTime(mAsleepTime, time);
+	    intent.putExtra(SleepLogHelper.ASLEEP_TIME, time);	    
+	}
+	else {
+	    mSleepLogHelper.updateAwakeTime(mAsleepTime, time);
+	    intent.putExtra(SleepLogHelper.AWAKE_TIME, time);	    
+	}
+	setResult(RESULT_OK, intent);
+    }
 	
-	public void onClickSave(View view) {
-		GregorianCalendar cal = new GregorianCalendar(mDatePicker.getYear(), mDatePicker.getMonth(),
-				mDatePicker.getDayOfMonth(), mTimePicker.getCurrentHour(), mTimePicker.getCurrentMinute());
-		long time = cal.getTimeInMillis();
-		if (mIsSleepTime) {
-			mSleepLogHelper.updateAsleepTime(mAsleepTime, time);
-			Intent intent = new Intent();
-			intent.putExtra(SleepLogHelper.ASLEEP_TIME, time);
-			setResult(RESULT_OK, intent);
-		} else {
-			mSleepLogHelper.updateAwakeTime(mAsleepTime, time);
-			Intent intent = new Intent();
-			intent.putExtra(SleepLogHelper.AWAKE_TIME, time);
-			setResult(RESULT_OK, intent);
-		}
-		finish();
-	}
-	
-	// For testing purposes
-	public void setIsSleepTime(boolean b) {
-		mIsSleepTime = b;
-	}
+    // For testing purposes
+    public void setIsSleepTime(boolean b) {
+	mIsSleepTime = b;
+    }
 }
